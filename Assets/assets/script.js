df7bf194ff96cd0513fc8430638851d54d8190b1
Daphnetatertot <daphnetatertot@gmail.com>
2022-11-16 13:12:05 -0500

    first commit

---
+++ Assets/assets/script.js
@@ -0,0 +1,21 @@
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton)
+
+    function showQuestions(questions, quizContainer){
+
+        //add code 
+    }
+
+    function showResults (questions, quizContainer, resultsContainer){
+
+        //add code
+    
+    }
+
+    // show questions, add click feature, display results
+
+    showQuestions(questions, quizContainer);
+
+    submitButton.onclick = function(){
+
+        showResults(questions,quizContainer, resultsContainer); 
+    }
