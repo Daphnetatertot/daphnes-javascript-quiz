8280586bb01e96c0167e285c4b589037fa085371
Daphnetatertot <daphnetatertot@gmail.com>
2022-11-16 17:16:40 -0500

    added place holders for questions

--- Assets/assets/script.js
+++ Assets/assets/script.js
@@ -3,8 +3,8 @@ var quizContainer = document.querySelector('#quiz-container')
 var questions = [
     {
         num: 1,
-        question: 'What is the meaning of life?',
-        answers: ['ans 1', 'ans 2', 'ans 3', 'ans 4',
+        question: 'What is JavaScripts main use?',
+        answers: ['styling', 'structure', 'function', 'all of the above',
 
         ],
 
@@ -13,7 +13,99 @@ var questions = [
     // reapeat above 5 times
     {
         num: 2,
+        question: 'What is the correct syntax for setting a variable?'
+        answers: ['var is', 'var =', 'function()', 'var', 
 
+    ],
+
+
+    }
+
+    correct: 'var',
+
+    {
+        num: 3,
+        question: 'add question here'
+        answers: ['ans 1', 'ans 2', 'ans 3', 'ans 4',
+    
+    ],
+    correct: 'ans 4',
+        
+    }
+
+    {
+        num: 4,
+        question: 'question 5',
+        answers: ['ans 1', 'ans 2', 'ans 3', 'ans 4',
+    
+    ],
+
+    correct: 'ans 1',
+
+    }
+
+    {
+        num: 5,
+        question: 'question 5',
+        answers: ['ans 1', 'ans 2', 'ans 3', 'ans 4',
+    
+    ],
+
+    correct: 'ans 3',
+
+    }
+
+    {
+        num: 6,
+        question: 'question 6',
+        answeres: ['ans 1', 'ans 2', 'ans 3', 'ans 4',
+    
+    ],
+
+correct: 'ans 4'
+
+    }
+
+    {
+
+        num: 7,
+        question: 'question 7',
+        answers: ['ans 1', 'ans 2', 'ans 3', 'ans 4',
+    
+    ],
+
+    correct: 'ans 4'
+    }
+
+    {
+        num: 8,
+        question: 'question 8',
+        answers: ['ans 1', 'ans 2', 'ans 3', 'ans 4',
+    
+    ],
+
+correct: 'ans 2',
+
+    }
+
+    {
+        num: 9,
+        question: 'question 9',
+        answers: ['ans 1', 'ans 2', 'ans 3', 'ans 4',
+    
+    ],
+
+    correct : 'ans 2'
+    }
+
+    {
+        num: 10,
+        question: 'question 10',
+        answers: ['ans 1', 'ans 2', 'ans 3', 'ans 4',
+    
+    ],
+
+    correct: 'ans 1'
     }
 ]
 
@@ -38,7 +130,7 @@ function showQuestions() {
 
 function showResults() {
     console.log('showing results');
-    
+
 
     //add code
 
